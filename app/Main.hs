cdebc054b8822563b39fc3523efdf47795e72505
Alexander Goussas <goussasalexander@gmail.com>
2022-09-25 18:57:24 -0500

    cabal: add missing bounds

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,12 @@ import           Scout.Options
 import           Scout.Requests
 
 import           Control.Lens   ((^.))
+import qualified Data.Text.IO   as TIO
 
 run :: Options -> IO ()
 run opts = case opts^.optCommand of
             SearchCommand searchOptions -> do
+                TIO.putStrLn $ "Searching for \"" <> (searchOptions^.searchQuery) <> "\"..."
                 packages <- searchPackagesWithInfo
                                 (searchOptions^.searchSortDirection)
                                 (searchOptions^.searchQuery)
